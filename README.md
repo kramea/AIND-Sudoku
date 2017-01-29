1d138902c8a3c42a576b5c88816f36d775e7b4c1
Kalai Ramea <kramea@ucdavis.edu>
2017-01-29 09:16:09 -0800

    Solved project
    Ready to submit

--- README.md
+++ README.md
@@ -3,11 +3,21 @@
 
 # Question 1 (Naked Twins)
 Q: How do we use constraint propagation to solve the naked twins problem?  
-A: *Student should provide answer here*
+
+A: The approach is similar to the 'eliminate' strategy, except that in this case, we are identifying pairs of 2-digit values, and are eliminating those digits from the rest of the 'unit' group (columns, rows, square units, diagonals) Here are the steps that was following to implement this:
+	1. Identify all the 2-digit values in the given grid
+	2. Construct a nested for loop with the twin value list to identify if there is a match
+	3. If yes, loop through each set of unit groups (columns, rows, squares, left-to-right diagonals, and right-to-left diagonals), and check if the matched pair exists in the unit group. 
+	4. If the pair exists, then remove the digits of the values of matched pair from the rest of the values in the unit group (sanity check: length of the values are checked. This operation is carried out only if the length is more than 1).
+	5. Iterate the loop for all the unit groups
 
 # Question 2 (Diagonal Sudoku)
 Q: How do we use constraint propagation to solve the diagonal sudoku problem?  
-A: *Student should provide answer here*
+A: A constraint on diagonals that they should have unique values is similar to the constraint we already have on regular Sudoku on column units, row units and square units.
+
+So, in order to implement this, two sets of diagonals are created: Left-to-right (A1, B2, C3....I9), and Right-to-left (I1, H2, G3,....A9). They are zipped to their respective values to create diagonal dictionaries.
+
+In order to implement diagonal sudoku, an additional constraint on unit list is made in all the three solve strategies: eliminate, only_choice and naked_twins. The unit list which previously consisted of rows, columns and squares now have diagonals, and the constraint makes sure each unit list consist of unique values.
 
 ### Install
 
--- solution.py
+++ solution.py
@@ -1,5 +1,36 @@
 assignments = []
 
+def cross(A, B):
+    #"Cross product of elements in A and elements in B."
+    return [s+t for s in A for t in B]
+
+## A few global variables are defined that would be used in the functions
+
+rows = 'ABCDEFGHI'
+cols = '123456789'
+boxes = cross(rows, cols) 
+
+diagonal_ltr = [ rows[i] + cols[i] for i in range(9)] # List of diagonal box addresses, starting from left to right (A1, B2,..)
+diagonal_rtl = [ rows[8-i] + cols[i] for i in range(9)] # List of diagonal box addresses, starting from right to left (I1, H2,..)
+
+row_units = [cross(r, cols) for r in rows] # List of row addresses
+column_units = [cross(rows, c) for c in cols] # List of column addresses
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')] # List of 3X3 squares
+
+diagonal_ltr = [ rows[i] + cols[i] for i in range(9)] # List of diagonal box addresses, starting from left to right (A1, B2,..)
+diagonal_rtl = [ rows[8-i] + cols[i] for i in range(9)] # List of diagonal box addresses, starting from right to left (I1, H2,..)
+
+unitlist = row_units + column_units + square_units # Nested list of all the units in the grid
+
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes) # Dictionary of all the units
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes) # Peers of all the units in the grid
+
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes) # Dictionary of all the units
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes) # Peers of all the units in the grid
+
+diag_ltr = dict((s, set(diagonal_ltr) - set([s])) for s in boxes if s in diagonal_ltr) # Diagonal peer set 1: Left to Right
+diag_rtl = dict((s, set(diagonal_rtl) - set([s])) for s in boxes if s in diagonal_rtl) # Diagonal peer set 2: Right to Left
+
 def assign_value(values, box, value):
     """
     Please use this function to update your values dictionary!
@@ -10,33 +41,47 @@ def assign_value(values, box, value):
         assignments.append(values.copy())
     return values
 
+
 def naked_twins(values):
-    """Eliminate values using the naked twins strategy.
-    Args:
-        values(dict): a dictionary of the form {'box_name': '123456789', ...}
 
-    Returns:
-        the values dictionary with the naked twins eliminated from peers.
-    """
+    # Finds all the boxes with two digit possibilities
+    twins = [box for box in values.keys() if len(values[box]) == 2]
 
-    # Find all instances of naked twins
-    # Eliminate the naked twins as possibilities for their peers
+    # Among the twin set, this subroutine finds a pair that matches
+    # If there is a match it searches through the two diagonal sets
+    # to remove the digits in the twin set in the other boxes
+    len_twins = len(twins)
+    fullunitlist = row_units + column_units + square_units + diagonal_rtl + diagonal_ltr
 
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
+    for i in range(len(twins) - 1):
+        for j in range(i+1, len(twins)):
+            if values[twins[i]] == values[twins[j]]:
+                for peerunit in fullunitlist:
+                    if twins[i] in peerunit and twins[j] in peerunit:
+                        twin_digits = values[twins[i]]
+                        for u in peerunit:
+                            if values[u] == values[twins[i]]:
+                                pass
+                            elif len(values[u])>1:
+                                for d in twin_digits:
+                                    values[u] = values[u].replace(d,'')
+
+
+    return values
 
 def grid_values(grid):
-    """
-    Convert grid into a dict of {square: char} with '123456789' for empties.
-    Args:
-        grid(string) - A grid in string form.
-    Returns:
-        A grid in dictionary form
-            Keys: The boxes, e.g., 'A1'
-            Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
-    """
-    pass
+
+    # This function replaces the empty values with all the possibilities '123456789'
+    # Then zips the values with the respective cell id and returns a dictionary
+    new_values = []
+    for val in grid:
+        if val == ".":
+            new_values.append('123456789')
+        else:
+            new_values.append(val)
+
+    return(dict(zip(boxes, new_values)))
+
 
 def display(values):
     """
@@ -44,19 +89,134 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
 
 def eliminate(values):
-    pass
+
+    ## This function looks at the peers groups of each box and eliminates the redundancies
+
+    ## There are three peer groups: Two diagonal sets, and the 'regular' sudoku peer group--rows, columns and square units
+
+    ## First the solved values are identified from the length of their values (if length = 1, then it is a solved value)
+    ## Then each peer group is iterated to remove these solved values from the set
+
+
+    ## Set 1 & 2 : Diagonals from left to right [A1, B2, C3...I9] and right to left [I1, H2, ...A9]
+    diagonal_list = [diag_ltr, diag_rtl]
+
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for diagonals in diagonal_list:
+            if box in diagonals.keys():
+                for diag in diagonals[box]:
+                    values[diag] = values[diag].replace(digit, '')
+    
+
+    ## Set 3: Regular Sudoku Peer group: Rows, Columns and 3X3 square units
+
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'') 
+
+    
+    return values
 
 def only_choice(values):
-    pass
+
+
+    ## This function checks the duplicates of possiblities in the peer group and isolates the 'only choice'
+    ## similar to 'eliminate' function, it searches the three peer groups in the puzzle
+    ## Three peer groups: Two diagonal groups and the regular sudoku peer group
+
+    ## It loops through every digit, keeps a digit counter. 
+    ## First, it loops through the peer group units, and the counter is updated when it finds the digit in the box
+    ## At the end of the loop, if the digit count is '1', then the cell which contains that value is assigned that digit
+
+    ## Set 1: Regular sudoku peer group
+                
+    for digit in '123456789':
+        for sq in unitlist:
+            digit_count = 0
+            for each_sq in sq:
+                if digit in values[each_sq]:
+                    digit_count = digit_count + 1
+            if digit_count == 1:
+                for each_sq in sq:
+                    if digit in values[each_sq]:
+                        values[each_sq] = digit 
+
+        ## Set 2: Diagonal group left to right: [A1, B2, ... I9]
+
+        digit_count = 0
+        for diag in diagonal_ltr:
+            if digit in values[diag]:
+                digit_count = digit_count + 1
+        if digit_count == 1:
+            for diag in diagonal_ltr:
+                if digit in values[diag]:
+                    values[diag] = digit
+
+
+        ## Set 3: Diagonal group right to left: [I1, H2, ... A9]
+
+        digit_count = 0
+        for diag in diagonal_ltr:
+            if digit in values[diag]:
+                digit_count = digit_count + 1
+        if digit_count == 1:
+            for diag in diagonal_ltr:
+                if digit in values[diag]:
+                    values[diag] = digit
+
+
+    return values
 
 def reduce_puzzle(values):
-    pass
+
+    ## This function takes the narrowed down possibilities and reduces the puzzle using the different strategies
+    ## The solved values are retained
+    ## Rest of the boxes go through the eliminate, only_choice and naken_twins strategies for reduction
+    ## The process is repeated until there cannot be any more reduction
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
+        values = only_choice(values)
+        values = naked_twins(values)
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
-    pass
+
+    ## Depth-first search is used to solve the sudoku grids that is obtained after reduction
+
+    values = reduce_puzzle(values) ## Reduces the puzzle until there is no choice to reduce further
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and 
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """
@@ -68,9 +228,44 @@ def solve(grid):
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
 
+    val = grid_values(grid) # uses the grid_values function to get the dictionary representation 
+    final_sudoku = search(val) ## Reduces the puzzle, and depth-first search is conducted until a solution is found
+
+    return final_sudoku
+
+
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
-    display(solve(diag_sudoku_grid))
+    
+
+    ## A few global variables are defined that would be used in the functions  
+    '''rows = 'ABCDEFGHI'
+    cols = '123456789'
+    boxes = cross(rows, cols) # List of box addresses in the sudoku grid
+ 
+    row_units = [cross(r, cols) for r in rows] # List of row addresses
+    column_units = [cross(rows, c) for c in cols] # List of column addresses
+    square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')] # List of 3X3 squares
+
+    diagonal_ltr = [ rows[i] + cols[i] for i in range(9)] # List of diagonal box addresses, starting from left to right (A1, B2,..)
+    diagonal_rtl = [ rows[8-i] + cols[i] for i in range(9)] # List of diagonal box addresses, starting from right to left (I1, H2,..)
+
+    unitlist = row_units + column_units + square_units # Nested list of all the units in the grid
+
+    units = dict((s, [u for u in unitlist if s in u]) for s in boxes) # Dictionary of all the units
+    peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes) # Peers of all the units in the grid
+
+    diag_ltr = dict((s, set(diagonal_ltr) - set([s])) for s in boxes if s in diagonal_ltr) # Diagonal peer set 1: Left to Right
+    diag_rtl = dict((s, set(diagonal_rtl) - set([s])) for s in boxes if s in diagonal_rtl) # Diagonal peer set 2: Right to Left'''
+
+    solved_puzzle = solve(diag_sudoku_grid) # Calls the solve function to obtain the solved puzzle
+
+    # If the sudoku grid is solved, then the output is visualized
+    # Else the program gives the output as 'False'
+    if solved_puzzle == False:
+        print(False)
+    else:
+        display(solved_puzzle)
 
     try:
         from visualize import visualize_assignments
